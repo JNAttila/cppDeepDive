02c5a9e769107159641c6adc9ad8722a0ea54496
JNAttila <juhasznagyattila@gmail.com>
2025-12-27 02:17:31 +0100

    implement Operator>> correctly

--- CppLearning/demo_proj/operatorSwap.cpp
+++ CppLearning/demo_proj/operatorSwap.cpp
@@ -17,29 +17,32 @@ public:
         data = d;
     }
 
-    void operator>>(A &obj) {
+    A &operator>>(A &obj) {
         const int tmp = obj.getData();
         obj.setData(this->data);
         this->data = tmp;
+        return obj;
     }
 };
 
-void printAll(const A &obj1, const A &obj2) {
+void printAll(const A &obj1, const A &obj2, const A &obj3) {
     cout << "OBJ1: " << obj1.getData() << endl;
     cout << "OBJ2: " << obj2.getData() << endl;
+    cout << "OBJ3: " << obj3.getData() << endl;
 }
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
     A a1(8);
     A a2(10);
+    A a3(20);
 
-    printAll(a1, a2);
+    printAll(a1, a2, a3);
 
     cout << "Operator >>" << endl;
-    a1 >> a2;
+    a1 >> a2 >> a3;
 
-    printAll(a1, a2);
+    printAll(a1, a2, a3);
 
     return 0;
 }
